9d10f99daf123b638ec042b06fb6f50db1ab6af4
Okole <okole@ifz-goettingen.de>
2025-11-07 11:59:23 +0100

    troubleshoot

--- src/app.py
+++ src/app.py
@@ -77,7 +77,7 @@ def download_from_drive(file_id, dest_path):
     #d = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"].replace("\\n", "\n"))
     #st.code(d)
     e = os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]
-    print(type(e), flush=True)
+    print(repr(e), flush=True)
     st.code(e)
     creds = service_account.Credentials.from_service_account_file(json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]))
     print("Cred: ", creds, flush=True)
